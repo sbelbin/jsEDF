2990123b810b6159a22add547161a3f32f20caa1
sbelbin <159043895+sbelbin@users.noreply.github.com>
2024-02-14 15:50:37 -0500

    Improvements for annotations

--- edf.js
+++ edf.js
@@ -1,31 +1,13 @@
-function flip_bits(value) {
-	return parseInt(value.toString(2).split('').map(bit => 1 - bit).join(''), 2);
-}
-
-function sample_value_endian(bytes) {
-	const value = (bytes.length == 1) ? bytes[0]
-		        : (bytes.length == 2) ? (bytes[1] << 8) + bytes[0]
-			    : (bytes.length == 3) ? (bytes[2] << 16) + (bytes[1] << 8) + bytes[0]
-				: (bytes.length == 4) ? (bytes[3] << 32) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0]
-				: undefined;
-
-	if (value && bytes[bytes.length - 1] >> 7 == 1) {
-		value = -flip_bits(value) - 1;
-	}
-
-	return value;
-}
-
-function computeCoefficient(signal)
-{
+function computeSignalScale(signal) {
 	return (signal.physicalMaximum - signal.physicalMinimum) / (signal.digitalMaximum - signal.digitalMinimum);
 }
 
-function allocateSignals(count)
-{
-	let signals = [];
+function allocateSignals(count) {
+	const signals = [];
+
 	for (var index = 0; index < count; index++) {
-		let signal = new Object();
+		const signal = {};
+		signal.annotations = [];
 		signal.records = [];
 		signals.push(signal);
 	}
@@ -33,8 +15,16 @@ function allocateSignals(count)
 	return signals;
 }
 
-function extractSignalSamplesInRange(signal, range)
-{
+function makeAllocation() {
+	const annotation = {};
+	annotation.onset = 0.0;
+	annotation.duration = 0.0;
+	annotation.notes = [];
+
+	return annotation;
+}
+
+function extractSignalSamplesInRange(signal, range) {
 	let records = signal.records.slice(range.from, range.until);
 	let samples = [];
 
@@ -44,81 +34,184 @@ function extractSignalSamplesInRange(signal, range)
 }
 
 class EDFParser {
-	constructor(uint8array) {
-		this.array = uint8array
-		this.byteOffset = 0
+	constructor(arrayBuffer) {
+		this.arrayBuffer = arrayBuffer;
+		this.byteOffset = 0;
+		this.byteLength = this.arrayBuffer.byteLength;
+		this.byteRemaining = this.byteLength;
+		this.data_view = new DataView(this.arrayBuffer);
+		this.decoder = new TextDecoder();
+		this.useLittleEndian = true;
+	}
+
+	advance(length) {
+		const byteCount = Math.min(length, this.byteRemaining);
+		this.byteRemaining -= byteCount;
+		this.byteOffset += byteCount;
+	}
+
+	checkRead(length) {
+		return (length <= this.byteRemaining);
 	}
 
 	readBytes(length) {
-		if (this.byteOffset + length > this.array.byteLength)
-		{
+		if (!this.checkRead(length)) {
 			return null;
 		}
 
-		const value = this.array.slice(this.byteOffset, this.byteOffset + length);
-		this.byteOffset += length;
+		const value = this.arrayBuffer.slice(this.byteOffset, this.byteOffset + length);
+		this.advance(length);
 		return value;
 	}
 
 	readText(length) {
-		const data = this.readBytes(length);
+		const text = this.decoder.decode(this.readBytes(length));
 
-		if (!data) {
+		return text?.trim();
+	}
+
+	readTextAsInt16(length = 8) {
+		return parseInt(this.readText(length));
+	}
+
+	readTextAsInt8(length = 4) {
+		return parseInt(this.readText(length));
+	}
+
+	readTextAsFloat32(length = 8) {
+		return parseFloat(this.readText(length));
+	}
+
+	readInt8() {
+		const length = 1;
+
+		if (!this.checkRead(length)) {
 			return null;
 		}
 
-		const decoder = new TextDecoder();
-		let value = decoder.decode(data);
-		value = value?.trim();
-
+		const value = this.data_view.getInt8(this.byteOffset);
+		this.advance(length);
 		return value;
 	}
 
-	readInt16(length = 8) {
-		const text = this.readText(length);
-		return parseInt(text);
-	}
+	readInt16() {
+		const length = 2;
 
-	readInt8(length = 4) {
-		const text = this.readText(length);
-		return parseInt(text);
-	}
-
-	readFloat32(length = 8) {
-		const text = this.readText(length);
-		return parseFloat(text);
-	}
-
-	readSampleValue(bitsSize) {
-		const useLittleEndian = true;
-		const data_view = new DataView(this.array.buffer, this.byteOffset, this.array.byteLength - this.byteOffset);
-
-		let value = undefined;
-
-		switch(bitsSize) {
-			case 8:
-				value = data_view.getInt8(0, useLittleEndian);
-				break;
-
-			case 16:
-				value = data_view.getInt16(0, useLittleEndian);
-				break;
-
-			case 24:
-				value = data_view.getInt16(0, useLittleEndian);
-				value << 8;
-				value += data_view.getInt8(2, useLittleEndian);
-				break;
+		if (!this.checkRead(length)) {
+			return null;
 		}
 
-		this.byteOffset += (bitsSize / 8);
-		return value
+		const value = this.data_view.getInt16(this.byteOffset, this.useLittleEndian);
+		this.advance(length);
+		return value;
+	}
+
+	readInt24() {
+		const length = 3;
+
+		if (!this.checkRead(length)) {
+			return null;
+		}
+
+		let value = this.readInt16();
+		value << 8;
+		value += this.readInt8();
+		return value;
+	}
+
+	readAnnotations(length) {
+		const MARKER_END_ANNOTATION = 0;
+		const MARKER_END_BLOCK = 20;
+		const MARKER_END_BLOCK_ONSET = 21;
+
+		const MODE_ONSET = 0;
+		const MODE_DURATION = 1;
+		const MODE_NOTES = 2;
+		const MODE_ANNOTATION_DONE = 3;
+
+		const data = new Uint8Array(this.readBytes(length));
+
+		const annotations = [];
+
+		let mode = MODE_ONSET;
+		let beginOffset = 0;
+		let annotation = null;
+		let decoder = this.decoder;
+
+		//
+		// Callback that is invoked at the end of each block is encountered.
+		//
+		function onEndBlock(dataOffset, setter)
+		{
+			const text = decoder.decode(data.slice(beginOffset, dataOffset))?.trim();
+
+			if (text) {
+				setter(text);
+			}
+
+			beginOffset = dataOffset + 1;
+		}
+
+		for (let dataOffset = 0; dataOffset < data.byteLength; dataOffset++) {
+
+			const currentByte = data[dataOffset];
+
+			if (currentByte === MARKER_END_ANNOTATION) {
+				if (mode === MODE_ANNOTATION_DONE) { // Exit loop when processing two consecutive end annotations (null character)
+					break;
+				}
+				mode = MODE_ANNOTATION_DONE;
+			} else if (mode === MODE_ANNOTATION_DONE) {
+				mode = MODE_ONSET;
+			}
+
+			switch (mode) {
+				case MODE_ONSET:
+					{
+						if (currentByte === MARKER_END_BLOCK || currentByte === MARKER_END_BLOCK_ONSET) {
+							annotation = makeAllocation();
+							onEndBlock(dataOffset, text => annotation.onset = parseFloat(text));
+							mode = (currentByte === MARKER_END_BLOCK_ONSET) ? MODE_DURATION : MODE_NOTES;
+						}
+					}
+					break;
+
+				case MODE_DURATION:
+					{
+						if (currentByte === MARKER_END_BLOCK) {
+							onEndBlock(dataOffset, text => annotation.duration = parseFloat(text));
+							mode = MODE_NOTES;
+						}
+					}
+					break;
+
+				case MODE_NOTES:
+					{
+						if (currentByte === MARKER_END_BLOCK) {
+							onEndBlock(dataOffset, text => annotation.notes.push(text));
+						}
+					}
+					break;
+
+				case MODE_ANNOTATION_DONE:
+					{
+						if (annotation) {
+							annotations.push(annotation);
+							annotation = null;
+						}
+						beginOffset = dataOffset + 1;
+					}
+					break;
+			}
+		}
+
+		return annotations;
 	}
 }
 
 class EDF {
 	constructor(uint8array) {
-		const parser = new EDFParser(uint8array);
+		const parser = new EDFParser(uint8array.buffer);
 
 		this.version = parser.readText(8);
 		this.patient = parser.readText(80);
@@ -126,73 +219,57 @@ class EDF {
 		this.recordingStartDate = parser.readText(8);
 		this.recordingStartTime = parser.readText(8);
 		this.headerBytesSize = parser.readText(8);
-		this.reserved = parser.readText(44); // EDF is 32 whereas EDF++ is 44.
+		this.reserved = parser.readText(44);
 
-		this.dataRecordsCount = parser.readInt16();
-		this.dataRecordDuration = parser.readInt16();
+		this.dataRecordsCount = parser.readTextAsInt16();
+		this.dataRecordDuration = parser.readTextAsFloat32();
 		this.recordingDuration = this.dataRecordDuration * this.dataRecordsCount;
+		this.samplesBytesSize = 2;
 
-		const signalsCount = parser.readInt8();
+		const signalsCount = parser.readTextAsInt8();
 
-		this.signals = allocateSignals(signalsCount);
-		this.signals.forEach((signal) => signal.label = parser.readText(16) );
-		this.signals.forEach((signal) => signal.transducer = parser.readText(80) );
-		this.signals.forEach((signal) => signal.dimensions = parser.readText(8) );
-		this.signals.forEach((signal) => signal.physicalMinimum = parser.readInt16() );
-		this.signals.forEach((signal) => signal.physicalMaximum = parser.readInt16() );
-		this.signals.forEach((signal) => signal.digitalMinimum = parser.readInt16() );
-		this.signals.forEach((signal) => signal.digitalMaximum = parser.readInt16() );
-		this.signals.forEach((signal) => signal.preFilters = parser.readText(80) );
-		this.signals.forEach((signal) => signal.samplesCountPerRecord = parser.readInt16() );
-		this.signals.forEach((signal) => signal.reserved = parser.readBytes(32) );
+		const signals = allocateSignals(signalsCount);
+		signals.forEach(signal => signal.label = parser.readText(16));
+		signals.forEach(signal => signal.transducer = parser.readText(80));
+		signals.forEach(signal => signal.dimensions = parser.readText(8));
+		signals.forEach(signal => signal.physicalMinimum = parser.readTextAsInt16());
+		signals.forEach(signal => signal.physicalMaximum = parser.readTextAsInt16());
+		signals.forEach(signal => signal.digitalMinimum = parser.readTextAsInt16());
+		signals.forEach(signal => signal.digitalMaximum = parser.readTextAsInt16());
+		signals.forEach(signal => signal.preFilters = parser.readText(80));
+		signals.forEach(signal => signal.samplesCountPerRecord = parser.readTextAsInt16());
+		signals.forEach(signal => signal.reserved = parser.readBytes(32));
 
-		this.signals.forEach((signal) => signal.coefficient = computeCoefficient(signal) );
+		signals.forEach(signal => signal.hasAnnotations = (signal.label.indexOf("DF Annotations") > 0));
+		signals.forEach(signal => signal.scale = computeSignalScale(signal));
 
-		this.hasAnnotations = false;
-		let annotationsCount = 0;
+		let readSampleValue = parser => parser.readInt16();
 
-		this.signals.forEach((signal) => {
-			signal.hasAnnotations = (signal.label.indexOf("DF Annotations") > 0);
-
-			if (signal.hasAnnotations) {
-				annotationsCount++;
-				this.hasAnnotations = true;
-			}
-		});
-
-		this.annotationBytesSize = 0;
-
-		if (this.hasAnnotations) {
-			this.samplingsCount += 60 * 2;
-
-			this.signals.forEach((signal) => {
-				if (signal.hasAnnotations) {
-					this.annotationBytesSize += signal.samplesCountPerRecord * 2;
-				}
-			});
+		if (this.samplesBytesSize === 3) {
+			readSampleValue = parser => parser.readInt24();
 		}
 
-		const sampleValueInBits = this.version == "0" ? 16 : 24;
-
 		for (var recordIndex = 0; recordIndex < this.dataRecordsCount; recordIndex++) {
-			this.signals.forEach((signal) => {
+			signals.forEach(signal => {
 				if (signal.hasAnnotations) {
-					const annotation = parser.readText(this.annotationBytesSize);
-					console.log("Annotation: ", annotation)
+					const annotations = parser.readAnnotations(signal.samplesCountPerRecord * this.samplesBytesSize);
+					signal.annotations.push(...annotations);
 				} else {
 					let samples = [];
 					for (var sampleIndex = 0; sampleIndex < signal.samplesCountPerRecord; sampleIndex++) {
-						const value = parser.readSampleValue(sampleValueInBits);
-						samples.push(value * signal.coefficient);
+						samples.push(readSampleValue(parser) * signal.scale);
 					}
 					signal.records.push(samples);
 				}
 			});
 		}
 
+		this.annotations = signals.filter(signal => signal.hasAnnotations);
+		this.signals = signals.filter(signal => !signal.hasAnnotations);
+
 		let samplesCountPerRecordMaximum = 0;
-		this.signals.forEach((signal) => samplesCountPerRecordMaximum = Math.max(samplesCountPerRecordMaximum, signal.samplesCountPerRecord));
-		this.samplingRate = this.recordingDuration / samplesCountPerRecordMaximum;
+		this.signals.forEach(signal => samplesCountPerRecordMaximum = Math.max(samplesCountPerRecordMaximum, signal.samplesCountPerRecord));
+		this.samplingRate = samplesCountPerRecordMaximum / this.dataRecordDuration;
 	}
 
 	computeSamplesRange(timeOffset, duration) {
@@ -219,9 +296,7 @@ class EDF {
 		let signalsSamples = [];
 
 		this.signals.forEach((signal) => {
-			if (!signal.hasAnnotations) {
-				signalsSamples.push(extractSignalSamplesInRange(signal, range));
-			}
+			signalsSamples.push(extractSignalSamplesInRange(signal, range));
 		});
 
 		return signalsSamples;
